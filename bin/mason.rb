c69fca41e17c158548dfa42ae8c0bb65c1befcea
Eric VanWieren <eric@codex.org>
2015-05-17 00:02:16 -0600

    Mason will now create a stack. It will use the inputs from the template as of now.

--- bin/mason.rb
+++ bin/mason.rb
@@ -10,6 +10,8 @@
 require 'aws-sdk'
 require 'inifile'
 require 'trollop'
+require 'yaml'
+require 'json'
 
 
 def get_aws_creds(global_opts)
@@ -21,14 +23,18 @@ def get_aws_creds(global_opts)
   return my_credentials
 end
 
+def get_cfn(credentials, region)
+  cfn = Aws::CloudFormation::Client.new(
+                                       region: region,
+                                       credentials: credentials
+  )
+  return cfn
+end
+
 def list(global_opts, cmd_opts)
   creds = get_aws_creds( global_opts )
   cmd_opts[:region] ? region = cmd_opts[:region] : region = 'us-east-1'
-
-  cfn = Aws::CloudFormation::Client.new(
-                                      region: region,
-                                      credentials: creds
-  )
+  cfn = get_cfn(creds, region)
 
   stacks = cfn.list_stacks(
       stack_status_filter: [:CREATE_IN_PROGRESS, :CREATE_FAILED, :CREATE_COMPLETE, :ROLLBACK_IN_PROGRESS, :ROLLBACK_FAILED, :ROLLBACK_COMPLETE, :DELETE_IN_PROGRESS, :DELETE_FAILED, :UPDATE_IN_PROGRESS, :UPDATE_COMPLETE_CLEANUP_IN_PROGRESS, :UPDATE_COMPLETE, :UPDATE_ROLLBACK_IN_PROGRESS, :UPDATE_ROLLBACK_FAILED, :UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS, :UPDATE_ROLLBACK_COMPLETE]
@@ -38,15 +44,52 @@ def list(global_opts, cmd_opts)
     puts "Stack: #{stack[:stack_name]}"
     puts "\tcreation time: #{stack[:creation_time]}"
     puts "\tstack status: #{stack[:stack_status]}"
-    puts
   end
-
-
-
-
-  exit(1)
 end
 
+def read_config( config_file )
+  return "foobar"
+end
+
+def create(global_opts, cmd_opts)
+  creds = get_aws_creds( global_opts )
+  cloudformation = File.read(cmd_opts[:cfn])
+  #cloudformation = JSON.parse(file)
+  cmd_opts[:region] ? region = cmd_opts[:region] : region = 'us-east-1'
+  cmd_opts[:config] ? params = read_config(cmd_opts[:config]) : params = Hash.new
+  cfn = get_cfn(creds, region)
+  resp = cfn.create_stack(
+      # required
+      stack_name: cmd_opts[:stack], # passed from command line.
+      template_body: cloudformation, # read this from file.
+      # template_url: "TemplateURL", # Use this is template is in public S3 Bucket
+
+      # Todo: pass in parameters
+      # This can be empty. So can pass in an empty variable
+      parameters: [ params ],
+      # parameters: [ # read these from a file
+      #     {
+      #         parameter_key: "ParameterKey",
+      #         parameter_value: "ParameterValue",
+      #         use_previous_value: true,
+      #     },
+      # ],
+      disable_rollback: true, # I like this.
+      timeout_in_minutes: 15,  # should bump this up to 15
+      #notification_arns: ["NotificationARN", '...'],
+      capabilities: ["CAPABILITY_IAM"], # '...'],
+      #on_failure: "DO_NOTHING", #"DO_NOTHING|ROLLBACK|DELETE",
+      # stack_policy_body: "StackPolicyBody",
+      # stack_policy_url: "StackPolicyURL",
+      tags: [
+          {
+              key: "TagKey",
+              value: "TagValue",
+          },
+      ],
+  )
+  puts "Create all the things."
+end
 
 if __FILE__ == $0
   SUB_COMMANDS = %w(create delete update list)
@@ -79,8 +122,10 @@ Usage:
      cfnmason create -c environment_config -s stack_name
 where [options] are:
                    EOS
-                   opt :config, "Environment configuration file", required: true
-                   opt :stack, "Stack to be created on AWS", required: true
+                   opt :config, "Environment configuration file", type: :string # todo: replace the stuff below with this
+                   opt :stack, "Stack to be created on AWS", required: true, type: :string
+                   opt :region, "Region to deplay stack", short: '-r', type: :string
+                   opt :cfn, "Cloud Formation Template", type: :string
                  end
                when "delete"  # needs to be done
                  Trollop::options do
@@ -97,12 +142,11 @@ where [options] are:
 
   if cmd == 'list'
     list(global_opts,cmd_opts)
+  elsif cmd == 'create'
+    create(global_opts, cmd_opts)
   end
-  puts "Global options: #{global_opts.inspect}"
-  puts "Subcommand: #{cmd.inspect}"
-  puts "Subcommand options: #{cmd_opts.inspect}"
-  puts "Remaining arguments: #{ARGV.inspect}"
-
-
-
+  # puts "Global options: #{global_opts.inspect}"
+  # puts "Subcommand: #{cmd.inspect}"
+  # puts "Subcommand options: #{cmd_opts.inspect}"
+  # puts "Remaining arguments: #{ARGV.inspect}"
 end
